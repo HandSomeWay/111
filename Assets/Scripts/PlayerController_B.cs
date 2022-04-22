6cb85135455d95f287518ecb4263e3a556532848
HandSomeWay <54880219+HandSomeWay@users.noreply.github.com>
2022-04-22 11:23:56 +0800

    move_B,change

--- Assets/Scripts/PlayerController_B.cs
+++ Assets/Scripts/PlayerController_B.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerController_B : MonoBehaviour
 {
-    public Rigidbody rb2;
 
+    public LayerMask whatStopsMovement;
+    public Transform movePoint;
     public float speed;
     // Start is called before the first frame update
     void Awake()
     {
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
@@ -27,14 +29,29 @@ public class PlayerController_B : MonoBehaviour
     }
     void Move()
     {
-        int dirVer,dirHor;
-        dirVer = dirHor = 0;
-        if (Input.GetKey(KeyCode.UpArrow)) dirVer = 1;
-        else if (Input.GetKey(KeyCode.DownArrow)) dirVer = -1;
-        if (Input.GetKey(KeyCode.RightArrow)) dirHor = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) dirHor = -1;
-
-        rb2.velocity = new Vector3(dirHor * speed, dirVer * speed, rb2.velocity.z);
-
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position,movePoint.position)<=0.5f)
+        {
+            int dirVer, dirHor;
+            dirVer = dirHor = 0;
+            if (Input.GetKey(KeyCode.UpArrow)) dirVer = 1;
+            else if (Input.GetKey(KeyCode.DownArrow)) dirVer = -1;
+            if (Input.GetKey(KeyCode.RightArrow)) dirHor = 1;
+            if (Input.GetKey(KeyCode.LeftArrow)) dirHor = -1;
+            if (Mathf.Abs(dirHor)==1f)
+            {
+                //if (Physics.OverlapSphere(movePoint.position + new Vector3(dirHor, 0f, 0f)+ new Vector3(0f, 0f, -1f), .2f, 1 << LayerMask.NameToLayer("whatStopsMovement")) == null)
+                {
+                    movePoint.position += new Vector3(dirHor, 0f, 0f);
+                }
+            }
+            else if(Mathf.Abs(dirVer)==1f)
+            {
+                //if (Physics.OverlapSphere(movePoint.position + new Vector3(dirVer, 0f, 0f)+ new Vector3(0f, 0f, -1f), .2f, 1<<LayerMask.NameToLayer("whatStopsMovement"))==null)
+                {
+                    movePoint.position += new Vector3(0f, dirVer, 0f);
+                }
+            }
+        }
     }
 }
