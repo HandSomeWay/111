2ca89cfca36750ef21ccee16f34d3a8c5ef97e72
LYG-1 <lyg1972019@gmail.com>
2022-04-27 23:08:09 +0800

    enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,9 +7,13 @@ public class EnemyController : MonoBehaviour
     private Rigidbody rb;
     public GameObject play1;
     public GameObject play2;
+    public GameObject enemy1;
+
     public float speed;//移动速度
+    public float rotat;//旋转方向
     public bool isGround;//是否在地面
     public float horizontalmove = 5;
+    float angle = 90f;
 
     void Start()
     {
@@ -18,7 +22,11 @@ public class EnemyController : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Wall")
+        {
             horizontalmove = -horizontalmove;
+            rotat = -rotat;//移动速度
+            enemy1.transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+        }
     }
     private void OnTriggerStay(Collider other)
     {
@@ -45,6 +53,8 @@ public class EnemyController : MonoBehaviour
 
     void MoveMode1()
     {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        enemy1.transform.rotation = Quaternion.Euler(0, rotat, 0);
         if (isGround)
         {
             rb.velocity = new Vector3(horizontalmove * speed, 0, 0);
@@ -53,8 +63,21 @@ public class EnemyController : MonoBehaviour
 
     void MoveMode2()
     {
+        transform.rotation = Quaternion.Euler(-90, 0, 0);
         rb.velocity = new Vector3(0, 0, 0);       
         var direction = play2.transform.position - transform.position;
+        
+        if (direction.x > 0.0001)
+        {
+            angle = 180f / Mathf.PI * Mathf.Asin(direction.y / direction.x);
+            enemy1.transform.localRotation = Quaternion.Euler(0, 90 - angle, 0);
+        }
+        if (direction.x < -0.0001)
+        {
+            angle = 180f / Mathf.PI * Mathf.Asin(direction.y / direction.x);
+            enemy1.transform.localRotation = Quaternion.Euler(0, -90 - angle, 0);
+        }
+
         transform.Translate(direction.normalized * Time.deltaTime * speed, Space.World);
     }
 }
