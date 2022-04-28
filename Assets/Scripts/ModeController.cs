fada95e1f2aea31c2e5f12b863a0399e6376935e
HandSomeWay <54880219+HandSomeWay@users.noreply.github.com>
2022-04-28 09:14:09 +0800

    ModeController添加日夜变化功能

--- Assets/Scripts/ModeController.cs
+++ Assets/Scripts/ModeController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class ModeController : MonoBehaviour
 {
-    static public bool ModeX;
+    static public bool ModeX;//游戏模式
+    static public bool GameOver;//游戏失败
+    static public bool Victory;//游戏胜利
+    private int i;
+    public Light SunLight;
     public GameObject PlayerA;
     public GameObject PlayerB;
     public Transform PA;
@@ -15,6 +19,8 @@ public class ModeController : MonoBehaviour
     private void Start()
     {
         ModeX = true;
+        GameOver = false;
+        Victory = false;
     }
     private void Update()
     {
@@ -22,21 +28,31 @@ public class ModeController : MonoBehaviour
         {
             Debug.Log("GameMode Changed!");
             ModeX = !ModeX;
+            StartCoroutine(LightChg());
         }
-        if (ModeX) 
+        if (ModeX) //模式转换时，另一维度的角色固定且透明化
         {
             PAT.parent = PA;
             PBT.parent = null;
-            PlayerA.active = true; 
             PlayerB.active = false;
+            PlayerA.active = true;
         }
         else
         {
             PAT.parent = null;
             PBT.parent = PB;
-            PlayerA.active = false; 
+            PlayerA.active = false;
             PlayerB.active = true;
         }
     }
-
+    IEnumerator LightChg()
+    {
+        for (int j = 1; j <= 60; j++)
+        {
+            SunLight.transform.Rotate(3f, 0, 0,Space.World);
+            //System.Threading.Thread.Sleep(10);
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return 0;
+    }
 }
