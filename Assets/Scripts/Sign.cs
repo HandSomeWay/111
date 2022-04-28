3a6c969291dface412a4f910bd6d0b0bb22e99e8
csyrich <88361506+csyrich@users.noreply.github.com>
2022-04-28 17:47:03 +0800

    新手教程对话

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public Text dialogBox;
+    void Start()
+    {
+        dialogBox.text = "快到我这来~\n按下A或D左右移动";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "NPC")
+        {
+            Debug.Log("Enter");
+            dialogBox.text = "试试跳跃【J】吧";
+            if (Input.GetKey(KeyCode.J))
+                dialogBox.text = "Bravo!";
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "NPC")
+        {
+            Debug.Log("Exit");
+            dialogBox.text = "";
+        }
+    }
+    
+}
