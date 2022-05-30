298bac52b5747b9d4ec01fe05e4dd300684f8d78
HandSomeWay <54880219+HandSomeWay@users.noreply.github.com>
2022-05-30 21:31:18 +0800

    添加部分2D角色资源，视角变换优化

--- Assets/Scripts/CameraChg.cs
+++ Assets/Scripts/CameraChg.cs
@@ -15,14 +15,45 @@ public class CameraChg : MonoBehaviour
     {
         if(ModeController.ModeX)
         {
-            this.gameObject.transform.position = new Vector3(0f, 3.65f, -10f);
-            this.gameObject.transform.localEulerAngles = new Vector3(15f, 0f, 0f);
+            //this.gameObject.transform.position = new Vector3(0f, -3f, -10f);
+            //this.gameObject.transform.localEulerAngles = new Vector3(-22f, 0f, 0f);
+            if(Input.GetKeyDown(KeyCode.X))
+            {
+                StartCoroutine(chg2());
+            }
+
 
         }
         else
         {
-            this.gameObject.transform.position = new Vector3(0f, 1f, -10f);
-            this.gameObject.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+            //this.gameObject.transform.position = new Vector3(0f, 1f, -10f);
+            //this.gameObject.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+            if(Input.GetKeyDown(KeyCode.X))
+            {
+                StartCoroutine(chg1());
+            }
         }
+    }
+    IEnumerator chg1()
+    {
+        float p = 0.4f, r = 2.2f;
+        for(int i=1;i<=10;i++)
+        {
+            this.gameObject.transform.position = new Vector3(0f, 1f - p * i, -10f);
+            this.gameObject.transform.localEulerAngles = new Vector3(-r * i, 0f, 0f);
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+    }
+    IEnumerator chg2()
+    {
+        float p = 0.4f, r = 2.2f;
+        for (int i = 1; i <= 10; i++)
+        {
+            this.gameObject.transform.position = new Vector3(0f, -3f + p * i, -10f);
+            this.gameObject.transform.localEulerAngles = new Vector3(-22f+ r * i, 0f, 0f);
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
     }
 }
