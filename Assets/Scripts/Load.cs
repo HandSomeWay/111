462ce1ad21f5fc3abf7165e1debc996dea89b420
W-binyu <1367798663@qq.com>
2022-06-02 00:17:07 +0800

    load

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value == 3)
+            SceneManager.LoadScene("Teach01");
+        slider.value += Time.deltaTime;
+    }
+}
