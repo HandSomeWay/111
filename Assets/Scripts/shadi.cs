948b9a2c74390b273087c30a87d9bbfb8467fa27
LYG-1 <lyg1972019@gmail.com>
2022-06-01 19:52:15 +0800

    shadi

--- Assets/Scripts/shadi.cs
+++ Assets/Scripts/shadi.cs
@@ -2,25 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class binkuai : MonoBehaviour
+public class shadi : MonoBehaviour
 {
     private Animator animator;
-    public GameObject bin; 
+
     // Start is called before the first frame update
     void Start()
     {
-        animator = bin.GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
-            animator.SetTrigger("diaoluo");
+            animator.SetTrigger("xianru");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            animator.SetTrigger("taisheng");
     }
 }
